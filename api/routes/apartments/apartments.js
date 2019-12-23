7b6f2d5d8c7dfd4ec50be2c7e8288364548d405b
Felipe Ares <felipeares@gmail.com>
2019-12-23 19:42:16 -0300

    API mocked Data: moved cities to own db file

--- api/routes/apartments/apartments.js
+++ api/routes/apartments/apartments.js
@@ -2,16 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/search/:id", async (req, res) => {
-  // MOCKED!
   // return random number of apartments
   const n = Math.floor(Math.random() * 100);
   const apartments = [];
   for (let i = 0; i <= n; i++) {
     apartments.push({
+      id: i,
+      title: "Lorem ipsum donor et grantes os binolom hermos",
       price: 50000 + Math.floor(Math.random() * 1000000),
       size: 20 + Math.floor(Math.random() * 500),
       rooms: 1 + Math.floor(Math.random() * 8),
-      bathrooms: 1 + Math.floor(Math.random() * 8)
+      bathrooms: 1 + Math.floor(Math.random() * 8),
+      image:
+        "https://m.foolcdn.com/media/millionacres/images/image1_BuYM1tG.width-1200.jpg"
     });
   }
   res.json(apartments);
--- api/routes/cities/cities.js
+++ api/routes/cities/cities.js
@@ -1,77 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const cities = [
-  "madrid",
-  "barcelona",
-  "valencia",
-  "sevilla",
-  "zaragoza",
-  "málaga",
-  "murcia",
-  "palma de mallorca",
-  "las palmas de gran canaria",
-  "bilbao",
-  "alicante",
-  "córdoba",
-  "valladolid",
-  "vigo",
-  "gijón",
-  "hospitalet de llobregat",
-  "vitoria",
-  "la coruña",
-  "granada",
-  "elche",
-  "oviedo",
-  "tarrasa",
-  "badalona",
-  "cartagena",
-  "jerez de la frontera",
-  "sabadell",
-  "móstoles",
-  "santa cruz de tenerife",
-  "pamplona",
-  "almería",
-  "alcalá de henares",
-  "fuenlabrada",
-  "leganés",
-  "san sebastián",
-  "getafe",
-  "burgos",
-  "albacete",
-  "santander",
-  "castellón de la plana",
-  "alcorcón",
-  "san cristóbal de la laguna",
-  "logroño",
-  "badajoz",
-  "huelva",
-  "salamanca",
-  "marbella",
-  "lérida",
-  "dos hermanas",
-  "tarragona",
-  "torrejón de ardoz",
-  "parla",
-  "mataró",
-  "león",
-  "algeciras",
-  "santa coloma de gramanet",
-  "cádiz",
-  "alcobendas",
-  "jaén",
-  "orense",
-  "reus",
-  "telde",
-  "baracaldo",
-  "gerona"
-];
+// HARD CODED!
+// TODO: Implement MongoDB or SQL DB
+const cities = require("../../db/cities");
 
 router.get("/search/:id", async (req, res) => {
   // TODO: work with tildes
-  const patt = new RegExp("^" + req.params.id.toLowerCase().trim());
+  const patt = new RegExp(req.params.id.toLowerCase().trim());
   const filtered = cities.filter(city => patt.test(city));
-
   res.json(filtered);
 });
 
