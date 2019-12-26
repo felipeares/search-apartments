81137530b354920419c9a97b309d09ed12785cc4
Felipe Ares <felipeares@gmail.com>
2019-12-26 12:45:28 -0300

    Images - serving static files - express static server - images upload

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const db = require("./db");
+const path = require("path");
 
 // middleware
 app.use(cors());
@@ -15,6 +16,7 @@ const citiesRouter = require("./routes/cities/cities");
 const apartmentsRouter = require("./routes/apartments/apartments");
 app.use("/cities", citiesRouter);
 app.use("/apartments", apartmentsRouter);
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 // start
 app.listen(9000, () => console.log("server started"));
