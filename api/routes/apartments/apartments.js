3f9ccbd4742760d8bd932f741082783ca38d8f6b
Felipe Ares <felipeares@gmail.com>
2019-12-26 12:30:08 -0300

    Apartments: Search mongodb for apartments filtered by city

--- api/routes/apartments/apartments.js
+++ api/routes/apartments/apartments.js
@@ -1,23 +1,17 @@
 const express = require("express");
+const Apartment = require("../../models/apartments");
 const router = express.Router();
 
 router.get("/search/:id", async (req, res) => {
-  // return random number of apartments
-  const n = Math.floor(Math.random() * 100);
-  const apartments = [];
-  for (let i = 0; i <= n; i++) {
-    apartments.push({
-      id: i,
-      title: "Lorem ipsum donor et grantes os binolom hermos",
-      price: 50000 + Math.floor(Math.random() * 1000000),
-      size: 20 + Math.floor(Math.random() * 500),
-      rooms: 1 + Math.floor(Math.random() * 8),
-      bathrooms: 1 + Math.floor(Math.random() * 8),
-      image:
-        "https://m.foolcdn.com/media/millionacres/images/image1_BuYM1tG.width-1200.jpg"
-    });
+  // only search for exact match
+  const city = req.params.id.toLowerCase().trim();
+  console.log(city);
+  try {
+    const apartments = await Apartment.find({ city: city });
+    res.json(apartments);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-  res.json(apartments);
 });
 
 module.exports = router;
